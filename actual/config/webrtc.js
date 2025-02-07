35af87cd369ce9894e24bad641dfc0ce5dcd9b63
khkhiu <khkhiu@gmail.com>
2025-02-07 15:21:19 +0800

    reorg

--- actual/config/webrtc.js
+++
@@ -1,9 +0,0 @@
-// config/webrtc.js
-export const createWebRTCConfig = (iceServerUrl, iceServerUsername, iceServerCredential, useTcpForWebRTC = false) => ({
-    iceServers: [{
-        urls: [ useTcpForWebRTC ? iceServerUrl.replace(':3478', ':443?transport=tcp') : iceServerUrl ],
-        username: iceServerUsername,
-        credential: iceServerCredential
-    }],
-    iceTransportPolicy: useTcpForWebRTC ? 'relay' : 'all'
-});
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Avatar } from './components/Avatar';
+
+function App() {
+  return (
+    <div className="App">
+        <Avatar/>
+    </div>
+  );
+}
+
+export default App;
