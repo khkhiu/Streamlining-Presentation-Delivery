27f4fc36c6f54361a8e13ec289799bef9be99287
khkhiu <khkhiu@gmail.com>
2025-02-09 09:24:57 +0800

    install shadcn dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+// vite.config.ts
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
