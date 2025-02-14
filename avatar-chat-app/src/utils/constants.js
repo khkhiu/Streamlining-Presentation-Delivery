4ccb09aeb85a62b2c1458cc0dea9bfc4c6d0c77e
khkhiu <khkhiu@gmail.com>
2025-02-14 10:07:45 +0800

    Added nodejs code

---
+++ avatar-chat-app/src/utils/constants.js
@@ -0,0 +1,14 @@
+// src/utils/constants.js
+module.exports = {
+    SENTENCE_LEVEL_PUNCTUATIONS: ['.', '?', '!', ':', ';', '。', '？', '！', '：', '；'],
+    BYOD_DOC_REGEX: /\[doc(\d+)\]/g,
+    SUPPORTED_REGIONS: [
+      'westus2',
+      'westeurope',
+      'southeastasia',
+      'southcentralus',
+      'northeurope',
+      'swedencentral',
+      'eastus2'
+    ]
+  };
